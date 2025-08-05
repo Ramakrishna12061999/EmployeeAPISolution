2480ed3b0e7f179ad76038d5867acd17735c3f14
Ramakrishna12061999 <odugururamakrishna@gmail.com>
2025-08-05 17:25:32 +0530

    Added EmployeeController with dummy data

---
+++ EmployeeAPI/Controllers/EmployeeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var employees = new[]
+            {
+                new { Id = 1, Name = "John" },
+                new { Id = 2, Name = "Alice" }
+            };
+            return Ok(employees);
+        }
+    }
+}
